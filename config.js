e5247a002502cc73dced30d8a42163dc639ef7c3
Jan Simson <git@simson.io>
2024-12-02 21:40:10 +0800

    Add leaderboard example study

--- config.js
+++ config.js
@@ -3,12 +3,31 @@ window.studies = [
   {
     // Required
     // The title of your study
-    title: "Color Matching",
+    title: "Color Matching 🟠 🔵",
     // A short description, ideally getting people excited to take part
     description:
-      "Briefly describe what your study is about and why people should totally take part in it!",
+      "A small study, illustrating the basics of jsPsych.",
     // The URL to your study, this should be the relative path to its index.html file
     url: "studies/study-color-match/index.html",
+
+    // Optional fields
+    // The estimated duration of your study
+    duration: "1 minute",
+    // An image / logo to display on the card
+    image: "img/example.png",
+    // Whether to show the study within a frame
+    // (this will keep the website navigation visible, but makes debugging the study a bit harder)
+    useFrame: true,
+  },
+  {
+    // Required
+    // The title of your study
+    title: "Leaderboard Study 🏆️",
+    // A short description, ideally getting people excited to take part
+    description:
+      "A small example study showing how the leaderboard works.",
+    // The URL to your study, this should be the relative path to its index.html file
+    url: "studies/study-leaderboard/index.html",
 
     // Optional fields
     // The estimated duration of your study
